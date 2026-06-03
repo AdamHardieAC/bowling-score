efb80f335ae4a15899728736ae74e5ad11eddb14
AdamHardieAC <adam.hardie@arnoldclark.com>
2026-06-03 11:18:53 +0100

    all tests passing,
    Some refactorings, missing test cases and test refactoring to follow

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const calculateScore = (frames) => {
   const flattedFrames = frames.flat();
-  //[6,6,6,6]
-  //[3,3,3,3,3,3,3]
-
-  //NEXTTIME - Refactor previous/next score fetching
   return flattedFrames.reduce((total, currentThrow, currIdx) => {
-    // console.log(currIdx);
     const prevThrow = flattedFrames[currIdx - 1];
     const nextThrow = flattedFrames[currIdx + 1];
     const isLastThrowOfFrame = currIdx % 2 != 0;
@@ -14,33 +9,19 @@ const calculateScore = (frames) => {
     const isSpare =
       prevThrow + currentThrow === 10 && prevThrow !== 10 && !isLastFrame;
 
-    //fix perfect game
-    //test consecutive spares
     if (isStrike) {
-      let nextFrameTotal = [
-        flattedFrames[currIdx + 2],
-        flattedFrames[currIdx + 3],
-      ].reduce((frameTotal, current) => {
-        return (frameTotal += current);
-      }, 0);
+      let nextFrameTotal =
+        flattedFrames[currIdx + 2] +
+        flattedFrames[currIdx + (flattedFrames[currIdx + 2] === 10 ? 4 : 3)];
+
       console.log({
-        currentThrow,
         currIdx,
-        isStrike,
-        isSpare,
         nextFrameTotal,
         total: total + currentThrow + nextFrameTotal,
       });
       return total + currentThrow + nextFrameTotal;
     }
 
-    console.log({
-      currentThrow,
-      isStrike,
-      isSpare,
-      total:
-        total + currentThrow + (isLastThrowOfFrame && isSpare ? nextThrow : 0),
-    });
     return (
       total + currentThrow + (isLastThrowOfFrame && isSpare ? nextThrow : 0)
     );
@@ -48,3 +29,6 @@ const calculateScore = (frames) => {
 };
 
 module.exports = calculateScore;
+// Refactoring
+// Missing tests?
+// Test refactoring
