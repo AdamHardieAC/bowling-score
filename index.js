02332ced5e886d432276d58f6ed3470de54b7363
Adam Hardie <adam.hardie@arnoldclark.com>
2026-07-01 11:18:31 +0100

    Test refactoring + calculating score refactoring,
    removed flattening the frames array to just a single reduce statement

--- index.js
+++ index.js
@@ -1,34 +1,47 @@
+// const calculateScore = (frames) => {
+//   const flattedFrames = frames.flat();
+//   return flattedFrames.reduce((total, currentThrow, currIdx) => {
+//     const prevThrow = flattedFrames[currIdx - 1];
+//     const nextThrow = flattedFrames[currIdx + 1];
+//     const isLastThrowOfFrame = currIdx % 2 != 0;
+//     const isLastFrame = currIdx >= 18;
+//     const isStrike = !isLastThrowOfFrame && currentThrow === 10 && !isLastFrame;
+//     const isSpare =
+//       prevThrow + currentThrow === 10 && prevThrow !== 10 && !isLastFrame;
+
+//     if (isStrike) {
+//       let nextFrameTotal =
+//         flattedFrames[currIdx + 2] +
+//         flattedFrames[currIdx + (flattedFrames[currIdx + 2] === 10 ? 4 : 3)];
+
+//       console.log({
+//         currIdx,
+//         nextFrameTotal,
+//         total: total + currentThrow + nextFrameTotal,
+//       });
+//       return total + currentThrow + nextFrameTotal;
+//     }
+
+//     return (
+//       total + currentThrow + (isLastThrowOfFrame && isSpare ? nextThrow : 0)
+//     );
+//   }, 0);
+// };
 const calculateScore = (frames) => {
-  const flattedFrames = frames.flat();
-  return flattedFrames.reduce((total, currentThrow, currIdx) => {
-    const prevThrow = flattedFrames[currIdx - 1];
-    const nextThrow = flattedFrames[currIdx + 1];
-    const isLastThrowOfFrame = currIdx % 2 != 0;
-    const isLastFrame = currIdx >= 18;
-    const isStrike = !isLastThrowOfFrame && currentThrow === 10 && !isLastFrame;
-    const isSpare =
-      prevThrow + currentThrow === 10 && prevThrow !== 10 && !isLastFrame;
+  return frames.reduce((total, currentThrow, currIdx) => {
+    let throwTotal = currentThrow.reduce((acc, curr, idx) => acc + curr, 0);
 
-    if (isStrike) {
-      let nextFrameTotal =
-        flattedFrames[currIdx + 2] +
-        flattedFrames[currIdx + (flattedFrames[currIdx + 2] === 10 ? 4 : 3)];
+    const isFinalFrame = currIdx === 9;
+    const isSpare = throwTotal === 10 && !isFinalFrame;
 
-      console.log({
-        currIdx,
-        nextFrameTotal,
-        total: total + currentThrow + nextFrameTotal,
-      });
-      return total + currentThrow + nextFrameTotal;
+    if (isSpare) {
+      const firstOfNextFrame = frames[currIdx + 1][0];
+      throwTotal += firstOfNextFrame;
     }
-
-    return (
-      total + currentThrow + (isLastThrowOfFrame && isSpare ? nextThrow : 0)
-    );
+    return total + throwTotal;
   }, 0);
 };
 
 module.exports = calculateScore;
-// Refactoring
-// Missing tests?
-// Test refactoring
+//Continue refactoring
+// work on strikes
